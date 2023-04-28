3b9749a1e9f2de3c841775bbbcdcc50b8debabe2
C_wangchenguang <chenguang.wang2@pactera.com>
2023-04-28 17:46:00 +0800

    230428

--- GoStudy/06ConcurrentProgramming/ctx.go
+++ GoStudy/06ConcurrentProgramming/ctx.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"runtime"
+	"sync"
 	"time"
+
+	"github.com/tal-tech/go-zero/core/contextx"
 )
 
 func main() {
@@ -17,12 +22,60 @@ func main() {
 	//go toTimeOut()
 	//select {}
 
-	ctx, _ := context.WithTimeout(context.TODO(), 1e9)
+	//ctx, _ := context.WithTimeout(context.TODO(), 1e9)
+	//
+	//go TCancelWithCtx(ctx)
+	//
+	//time.Sleep(2e9)
+	//fmt.Println(ctx.Err())
 
-	go TCancelWithCtx(ctx)
+	// 第一版context超时实现
+	const total = 1000
+	var wg sync.WaitGroup
+	wg.Add(total)
+	now := time.Now()
+	for i := 0; i < total; i++ {
+		go func() {
+			defer wg.Done()
+			ctx, _ := context.WithTimeout(context.Background(), 1e9)
+			requestWork(ctx, "any")
+		}()
+	}
+	wg.Wait()
+	fmt.Println("elapsed:", time.Since(now))
+	// 查看Goroutines泄露问题  总的来说,下游的服务没有被中断,只是上游结束了
+	fmt.Println("number of goroutines:", runtime.NumGoroutine())
+}
 
-	time.Sleep(2e9)
-	fmt.Println(ctx.Err())
+func requestWork(ctx context.Context, job interface{}) error {
+	ctx, f := contextx.ShrinkDeadline(ctx, 2e9)
+	defer f()
+
+	done := make(chan error, 1)
+	panicChan := make(chan interface{}, 1)
+	go func() {
+		defer func() {
+			if p := recover(); p != nil {
+				panicChan <- p
+			}
+		}()
+		done <- hardWork(job)
+	}()
+
+	select {
+	case err := <-done:
+		return err
+	case p := <-panicChan:
+		fmt.Println(p)
+		return errors.New(p.(string))
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+}
+
+func hardWork(job interface{}) error {
+	time.Sleep(time.Minute)
+	return nil
 }
 
 func TCancelWithCtx(ctx context.Context) {
--- GoStudy/06ConcurrentProgramming/go.mod
+++ GoStudy/06ConcurrentProgramming/go.mod
@@ -2,4 +2,9 @@ module 06ConcurrentProgramming
 
 go 1.17
 
-require golang.org/x/sync v0.1.0
+require (
+	github.com/tal-tech/go-zero v1.0.1
+	golang.org/x/sync v0.1.0
+)
+
+require gopkg.in/yaml.v2 v2.2.8 // indirect
