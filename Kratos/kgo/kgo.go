414ea243961b151ccbf03a064666566c49bc38f4
C_wangchenguang <chenguang.wang2@pactera.com>
2023-10-31 18:29:56 +0800

    231031

---
+++ Kratos/kgo/kgo.go
@@ -0,0 +1,26 @@
+package main
+
+func main() {
+	//err := trpc.Go(context.TODO(), 1*time.Second, func(ctx context.Context) {
+	//	time.Sleep(5e9)
+	//	_, ok := ctx.Deadline()
+	//	if ok {
+	//		fmt.Println("ctx cancel")
+	//	}
+	//	fmt.Println("i'm go")
+	//})
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//select {}
+
+	//value, found := syscall.Getenv("MySQL")
+	//if found {
+	//	fmt.Println(value)
+	//}
+	//fmt.Println(value)
+
+	//env := trpc.ExpandEnv("${MySQL}")
+	//fmt.Println(env)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module Study
 
 go 1.19
+
+require github.com/go-kratos/kratos v1.0.1 // indirect
